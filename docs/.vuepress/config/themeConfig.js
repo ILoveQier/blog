08916c526f3eeefcff897a4978aad0c33db0bb38
王超 <597584896@qq.com>
2022-08-25 22:20:37 +0800

    ggg

--- docs/.vuepress/config/themeConfig.js
+++ docs/.vuepress/config/themeConfig.js
@@ -1,3 +1,8 @@
+/*
+ * @Description: 
+ * @Author: wangchao
+ * @Date: 2022-08-25 21:51:28
+ */
 const nav = require('./themeConfig/nav.js');
 // const sidebar = require('./themeConfig/sidebar.js');
 
@@ -40,8 +45,8 @@ module.exports = {
   },
   author: {
     // 文章默认的作者信息，可在md文件中单独配置此信息 String | {name: String, link: String}
-    name: '二丫讲梵', // 必需
-    link: 'https://github.com/eryajf', // 可选的
+    name: '其二', // 必需
+    link: 'https://github.com/ILoveQier', // 可选的
   },
   social: {
     // 社交图标，显示于博主信息栏和页脚栏
@@ -50,17 +55,17 @@ module.exports = {
       {
         iconClass: 'icon-github',
         title: 'GitHub',
-        link: 'https://github.com/eryajf',
+        link: 'https://github.com/ILoveQier',
       },
       {
         iconClass: 'icon-youjian',
         title: '发邮件',
-        link: 'mailto:eryajf@163.com',
+        link: 'mikewang199333@gmail.com',
       },
       {
         iconClass: 'icon-gitee',
         title: 'Gitee',
-        link: 'https://gitee.com/eryajf',
+        link: 'https://gitee.com/wangchaoandyuxia',
       },
     ],
   },
@@ -68,6 +73,6 @@ module.exports = {
     // 页脚信息
     createYear: 2022, // 博客创建年份
     copyrightInfo:
-      'Eryajf | <a href="https://github.com/eryajf/blog/blob/main/LICENSE" target="_blank">MIT License</a>', // 博客版权信息，支持a标签
+      'Qier | <a href="https://github.com/eryajf/blog/blob/main/LICENSE" target="_blank">MIT License</a>', // 博客版权信息，支持a标签
   }
 }
